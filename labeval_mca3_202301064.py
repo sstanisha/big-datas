feec03e5d061036241601336812458085f3dc773
sstanisha <161505817+sstanisha@users.noreply.github.com>
2024-04-30 12:08:28 +0530

    labeval_mca3_202301064.py

--- labeval_mca3_202301064.py
+++ labeval_mca3_202301064.py
@@ -1,28 +1,22 @@
+import numpy as np
 import pandas as pd
-data=pd.read_csv('company_sales.csv')
-print(data)
-
 import matplotlib.pyplot as plt
-#d=pd.read('facecream')
-plt.bar('facecream','facecream')
-plt.bar('facewash','facewash')
+import io
+import seaborn as sns
+data=pd.read_csv("company_sales.csv")
+plt.bar(data["month_number"],data["facecream"],label="facecream")
+plt.bar(data["month_number"],data["facewash"],label="facewash")
+plt.xlabel('month')
+plt.ylabel('sales')
+plt.title("Bar chart")
+plt.legend()
 plt.show()
-        
-import matplotlib.pyplot as plt
-month_number=[1,2,3,4,5,6,7,8,9,10,11,12]
-total_profit=[
-211000,
-183300,
-224700,
-22700,
-209600,
-201400,
-295500,
-361400,
-23400,
-266700,
-412800,
-300200,
-]
-plt.hist('month_number','total_profit')
+monthList= data['month_number'].tolist()
+faceCremSalesData=data['facecream'].tolist()
+faceWashSalesData=data['facewash'].tolist()
+plt.bar([a-0.25 for a in monthList], faceCremSalesData, width= 0.25)
+plt.bar([a+0.25 for a in monthList], faceWashSalesData, width= -0.25)
+plt.xticks(monthList)
+plt.xlabel('month list')
+plt.ylabel('total sales')
 plt.show()
